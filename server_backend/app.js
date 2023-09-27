c1f3c11c45941bcad59734d5e35182308c253101
amilkarhernandez <amilkarhernandez07@gmail.com>
2023-09-26 19:32:54 -0500

    :boom: configuration project

---
+++ server_backend/app.js
@@ -0,0 +1,48 @@
+
+//Librerias
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+//Datos codifcados en URL
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Analiza objeto Json
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+app.get('/status', (req, res) => {
+    res.status(200).send({
+      success: 'true',
+      message: 'Servidor Corriendo'
+    })
+});
+
+
+//Conexion a base de datos
+let MONGODB_URI = `mongodb://${process.env.BD_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.MONGO_DB}?retryWrites=true&authSource=admin`;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Base de Datos Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  });
+
+//Export
+module.exports = app;
---
+++ server_backend/index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const app = require("./app");
+require("dotenv").config();
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, ()=>{
+    console.log(`Server Up, Port: ${process.env.PORT}`);
+})
