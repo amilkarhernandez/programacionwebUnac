9fb44d2c01c22a16af56a0d1a049f8cb402ef5a4
amilkarhernandez <amilkarhernandez07@gmail.com>
2023-11-07 22:07:58 -0500

    :boom: check list pruebas

--- server_backend/src/test/UsuariosRepository.test.js
+++ server_backend/src/test/UsuariosRepository.test.js
@@ -1,5 +1,5 @@
 const UserModel = require("../models/UsuariosModels");
-const { FindAllUser, FindOneUser, CreateUser } = require("../repository/UserRepository");
+const { FindAllUser, FindOneUser, CreateUser, deleteUser, updateUser } = require("../repository/UserRepository");
 
 describe("Test Usuarios", () => {
   beforeEach(() => {
@@ -101,6 +101,76 @@ describe("Test Usuarios", () => {
     } catch (error) {
       expect(error.status).toBe(500);
       expect(error.message).toBe("Ocurrio un error en el servidor");
+    }
+  });
+
+  it("Shoul DeleteUser response ok", async () => {
+    const userId = "123";
+    const user = { _id: userId, name: "User1", usuario: "user1" };
+
+    jest
+      .spyOn(UserModel, "findByIdAndDelete")
+      .mockImplementationOnce((userId) => Promise.resolve(user));
+
+    const result = await deleteUser(userId);
+
+    expect(result.status).toBe(200);
+    expect(result.message).toBe("Registro Eliminado correctamente");
+    expect(result.result).toEqual(user);
+  });
+
+  it("Shoul deleteUser response Fail", async () => {
+    const userId = "123";
+    const expectedErrorData = { errorMessage: "test error scenario" };
+
+    jest
+      .spyOn(UserModel, "findByIdAndDelete")
+      .mockImplementationOnce((userId) =>
+        Promise.reject(new Error(expectedErrorData))
+      );
+
+    try {
+      await deleteUser(userId);
+    } catch (error) {
+      expect(error.status).toBe(500);
+    }
+  });
+
+  it("Shoul updateUser response ok", async () => {
+    const userId = "654afad180c86c12c0c29d14";
+    
+    const user = new UserModel();
+    user.nombres = "test nombres";
+    user.apellidos = "test apellidos";
+
+    jest
+      .spyOn(UserModel, "findOneAndUpdate")
+      .mockImplementationOnce((userId, user) => Promise.resolve(user));
+
+    const result = await updateUser(userId, user);
+
+    expect(result.status).toBe(200);
+    expect(result.message).toBe("Registro Actualizado correctamente");
+  });
+
+  it("Shoul updateUser response Fail", async () => {
+    const userId = "654afad180c86c12c0c29d14";
+    const expectedErrorData = { errorMessage: "test error scenario" };
+
+    const user = new UserModel();
+    user.nombres = "test nombres";
+    user.apellidos = "test apellidos";
+
+    jest
+      .spyOn(UserModel, "findOneAndUpdate")
+      .mockImplementationOnce((userId, user) =>
+        Promise.reject(new Error(expectedErrorData))
+      );
+
+    try {
+      await updateUser(userId, user);
+    } catch (error) {
+      expect(error.status).toBe(500);
     }
   });
 });
